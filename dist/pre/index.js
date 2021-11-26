4a6be8b1004f67d87cbfbf9fd88a1bb5d11730e4
Varun Sharma <varunsh@stepsecurity.io>
2021-11-25 16:25:00 -0800

    Remove color

--- dist/pre/index.js
+++ dist/pre/index.js
@@ -2373,7 +2373,7 @@ const parse = dist/* parse */.Qc;
 ;// CONCATENATED MODULE: ./src/common.ts
 function printInfo(web_url) {
     console.log("View security insights and recommended policy at:");
-    console.log("\x1b[32m%s\x1b[0m", `${web_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}`);
+    console.log(`${web_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}`);
 }
 
 ;// CONCATENATED MODULE: ./src/setup.ts
