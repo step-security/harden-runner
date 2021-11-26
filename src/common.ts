3d98d1a270ad31f4809584db7ad8010a8ffba31f
Varun Sharma <varunsh@stepsecurity.io>
2021-11-25 16:20:02 -0800

    Change foreground color

--- src/common.ts
+++ src/common.ts
@@ -1,7 +1,7 @@
 export function printInfo(web_url) {
   console.log("View security insights and recommended policy at:");
   console.log(
-    "\x1b[44m%s\x1b[0m",
+    "\x1b[34m%s\x1b[0m",
     `${web_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}`
   );
 }
