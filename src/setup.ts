d3f82b3c021697dd68a85fefbcce984927533206
Varun Sharma <varunsh@stepsecurity.io>
2021-12-22 07:01:39 -0800

    Update agent version

--- src/setup.ts
+++ src/setup.ts
@@ -54,7 +54,7 @@ import * as tc from "@actions/tool-cache";
     cp.execSync("sudo chown -R $USER /home/agent");
 
     const downloadPath: string = await tc.downloadTool(
-      "https://github.com/step-security/agent/releases/download/v0.5.0/agent_0.5.0_linux_amd64.tar.gz"
+      "https://github.com/step-security/agent/releases/download/v0.5.2/agent_0.5.2_linux_amd64.tar.gz"
     );
     const extractPath = await tc.extractTar(downloadPath);
 
