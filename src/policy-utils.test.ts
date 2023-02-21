42711212067bdc48f1c8d24a7efd45ddcd37711a
h0x0er <jatink843@protonmail.com>
2023-02-21 23:35:42 +0530

    added functionality to fetch policy

---
+++ src/policy-utils.test.ts
@@ -0,0 +1,66 @@
+import nock from "nock";
+import { API_ENDPOINT, fetchPolicy, mergeConfigs } from "./policy-utils";
+import { Configuration, PolicyResponse } from "./interfaces";
+
+test("success: fetching policy", async () => {
+  let owner = "h0x0er";
+  let policyName = "policy1";
+  let response = {
+    owner: "h0x0er",
+    policyName: "policy1",
+    allowed_endpoints: ["github.com:443"],
+    egress_policy: "audit",
+    disable_telemetry: false,
+    disable_sudo: false,
+    disable_file_monitoring: false,
+  };
+  const policyScope = nock(`${API_ENDPOINT}`)
+    .get(`/github/${owner}/actions/policies/${policyName}`)
+    .reply(200, response);
+
+  let policy = await fetchPolicy(owner, policyName);
+  console.log(policy);
+  expect(policy).toStrictEqual(response);
+});
+
+test("merge configs", async () => {
+  let localConfig: Configuration = {
+    repo: "test/repo",
+    run_id: "xyx",
+    correlation_id: "aaaaa",
+    working_directory: "/xyz",
+    api_url: "xyz",
+    allowed_endpoints: "",
+    egress_policy: "audit",
+    disable_telemetry: false,
+    disable_sudo: false,
+    disable_file_monitoring: false,
+    private: "true",
+  };
+  let policyResponse: PolicyResponse = {
+    owner: "h0x0er",
+    policyName: "policy1",
+    allowed_endpoints: ["github.com:443", "google.com:443"],
+    egress_policy: "audit",
+    disable_telemetry: false,
+    disable_sudo: false,
+    disable_file_monitoring: false,
+  };
+
+  let expectedConfiguration: Configuration = {
+    repo: "test/repo",
+    run_id: "xyx",
+    correlation_id: "aaaaa",
+    working_directory: "/xyz",
+    api_url: "xyz",
+    allowed_endpoints: "github.com:443 google.com:443",
+    egress_policy: "audit",
+    disable_telemetry: false,
+    disable_sudo: false,
+    disable_file_monitoring: false,
+    private: "true",
+  };
+
+  localConfig = mergeConfigs(localConfig, policyResponse);
+  expect(localConfig).toStrictEqual(expectedConfiguration);
+});
