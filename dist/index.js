49347f7145d00aecbbbeebe24fcabec4597a9f3d
Varun Sharma <varunsh@stepsecurity.io>
2023-06-18 11:54:49 -0700

    Update logic and add one test

--- dist/index.js
+++ dist/index.js
@@ -2852,86 +2852,86 @@ function printInfo(web_url) {
     console.log("\x1b[32m%s\x1b[0m", "View security insights and recommended policy at:");
     console.log(`${web_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}`);
 }
+const processLogLine = (line, tableEntries) => {
+    if (line.includes("pid") &&
+        line.includes("process") &&
+        line.includes("domain") &&
+        line.includes("ip address")) {
+        const matches = line.match(/ip address:port ([\d.:]+), domain: ([\w.-]+), pid: (\d+), process: (\w+)/);
+        if (matches) {
+            const [ipAddress, domain, pid, process] = matches.slice(1);
+            // Check if all values are non-empty
+            if (pid && process && domain && ipAddress) {
+                const status = ipAddress.startsWith("54.185.253.63")
+                    ? "❌ Blocked"
+                    : "✔️ Allowed";
+                tableEntries.push({ pid, process, domain, ipAddress, status });
+            }
+        }
+    }
+};
 function addSummary() {
     return __awaiter(this, void 0, void 0, function* () {
-        if (process.env.STATE_monitorStatusCode === "200") {
-            const web_url = "https://app.stepsecurity.io";
-            const insights_url = `${web_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}`;
-            yield core.summary
-                .addSeparator()
-                .addRaw(`<h2>🛡GitHub Actions Runtime Security</h2>
-        <p><a href="https://github.com/step-security/harden-runner">By StepSecurity Harden Runner</a></p>`);
-            const log = "/home/agent/agent.log";
-            if (fs.existsSync(log)) {
-                const content = fs.readFileSync(log, "utf-8");
-                const lines = content.split("\n");
-                let tableEntries = [];
-                for (const line of lines) {
-                    if (line.includes("PID") &&
-                        line.includes("Process") &&
-                        line.includes("Domain") &&
-                        line.includes("IP Address")) {
-                        const matches = line.match(/PID: (\d+), Process: (\w+), Domain: ([\w.-]+), IP Address: ([\d.]+)/);
-                        const pid = matches[1];
-                        const process = matches[2];
-                        const domain = matches[3];
-                        const ipAddress = matches[4];
-                        const status = "✔️ Allowed";
-                        tableEntries.push({ pid, process, domain, ipAddress, status });
-                    }
-                    else if (line.includes("domain not allowed")) {
-                        const matches = line.match(/domain not allowed: ([\w.-]+)/);
-                        const domain = matches[1];
-                        const foundEntry = tableEntries.find(entry => entry.domain === domain);
-                        if (foundEntry) {
-                            foundEntry.status = "❌ Blocked";
-                        }
-                    }
-                }
-                if (tableEntries.length > 0) {
-                    tableEntries.sort((a, b) => {
-                        if (a.status === "❌ Blocked" && b.status !== "❌ Blocked") {
-                            return -1;
-                        }
-                        else if (a.status !== "❌ Blocked" && b.status === "❌ Blocked") {
-                            return 1;
-                        }
-                        else {
-                            return 0;
-                        }
-                    });
-                    tableEntries = tableEntries.slice(0, 5); // Limit the table entries to 5
-                    yield core.summary.addRaw(`
-          <h3>🌐 Outbound Network Traffic Analysis</h3>
-          <table>
-            <thead>
-              <tr>
-                <th>PID</th>
-                <th>Process</th>
-                <th>Domain</th>
-                <th>IP Address</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              ${tableEntries
-                        .map(entry => `<tr>
-                  <td>${entry.pid}</td>
-                  <td>${entry.process}</td>
-                  <td>${entry.domain}</td>
-                  <td>${entry.ipAddress}</td>
-                  <td>${entry.status}</td>
-                </tr>`)
-                        .join("")}
-            </tbody>
-          </table>`);
-                }
-            }
-            yield core.summary
-                .addRaw(`<p>🔍 <a href="${insights_url}">View detailed insights and policy recommendation</a></p>`)
-                .addSeparator()
-                .write();
+        if (process.env.STATE_monitorStatusCode !== "200") {
+            return;
         }
+        const web_url = "https://app.stepsecurity.io";
+        const insights_url = `${web_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}`;
+        const log = "/home/agent/agent.log";
+        if (!fs.existsSync(log)) {
+            return;
+        }
+        const content = fs.readFileSync(log, "utf-8");
+        const lines = content.split("\n");
+        let tableEntries = [];
+        for (const line of lines) {
+            processLogLine(line, tableEntries);
+        }
+        if (tableEntries.length === 0) {
+            return;
+        }
+        yield core.summary.addSeparator().addRaw(`<h2>GitHub Actions Runtime Security</h2>
+        <p><a href="https://github.com/step-security/harden-runner">By StepSecurity Harden Runner</a></p>`);
+        tableEntries.sort((a, b) => {
+            if (a.status === "❌ Blocked" && b.status !== "❌ Blocked") {
+                return -1;
+            }
+            else if (a.status !== "❌ Blocked" && b.status === "❌ Blocked") {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        });
+        tableEntries = tableEntries.slice(0, 5); // Limit the table entries
+        yield core.summary.addRaw(`
+    <h3>🌐 Outbound Network Traffic Analysis</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>PID</th>
+          <th>Process</th>
+          <th>Domain</th>
+          <th>IP Address</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${tableEntries
+            .map((entry) => `<tr>
+            <td>${entry.pid}</td>
+            <td>${entry.process}</td>
+            <td>${entry.domain}</td>
+            <td>${entry.ipAddress}</td>
+            <td>${entry.status}</td>
+          </tr>`)
+            .join("")}
+      </tbody>
+    </table>`);
+        yield core.summary
+            .addRaw(`<p>🔍 <a href="${insights_url}">View detailed insights and policy recommendation</a></p>`)
+            .addSeparator()
+            .write();
     });
 }
 const STATUS_HARDEN_RUNNER_UNAVAILABLE = "409";
--- src/common.ts
+++ src/common.ts
@@ -12,99 +12,116 @@ export function printInfo(web_url) {
   );
 }
 
-export async function addSummary() {
-  if (process.env.STATE_monitorStatusCode === "200") {
-    const web_url = "https://app.stepsecurity.io";
-    const insights_url = `${web_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}`;
+export const processLogLine = (
+  line: string,
+  tableEntries: {
+    pid: string;
+    process: string;
+    domain: string;
+    ipAddress: string;
+    status: string;
+  }[]
+): void => {
+  if (
+    line.includes("pid") &&
+    line.includes("process") &&
+    line.includes("domain") &&
+    line.includes("ip address")
+  ) {
+    const matches = line.match(
+      /ip address:port ([\d.:]+), domain: ([\w.-]+), pid: (\d+), process: (\w+)/
+    );
+    if (matches) {
+      const [ipAddress, domain, pid, process] = matches.slice(1);
 
-    await core.summary
-      .addSeparator()
-      .addRaw(
-        `<h2>🛡GitHub Actions Runtime Security</h2>
-        <p><a href="https://github.com/step-security/harden-runner">By StepSecurity Harden Runner</a></p>`
-      );
+      // Check if all values are non-empty
+      if (pid && process && domain && ipAddress) {
+        const status = ipAddress.startsWith("54.185.253.63")
+          ? "❌ Blocked"
+          : "✔️ Allowed";
 
-    const log = "/home/agent/agent.log";
-    if (fs.existsSync(log)) {
-      const content = fs.readFileSync(log, "utf-8");
-      const lines = content.split("\n");
-
-      let tableEntries = [];
-
-      for (const line of lines) {
-        if (
-          line.includes("PID") &&
-          line.includes("Process") &&
-          line.includes("Domain") &&
-          line.includes("IP Address")
-        ) {
-          const matches = line.match(/PID: (\d+), Process: (\w+), Domain: ([\w.-]+), IP Address: ([\d.]+)/);
-          const pid = matches[1];
-          const process = matches[2];
-          const domain = matches[3];
-          const ipAddress = matches[4];
-          const status = "✔️ Allowed";
-
-          tableEntries.push({ pid, process, domain, ipAddress, status });
-        } else if (line.includes("domain not allowed")) {
-          const matches = line.match(/domain not allowed: ([\w.-]+)/);
-          const domain = matches[1];
-
-          const foundEntry = tableEntries.find(entry => entry.domain === domain);
-          if (foundEntry) {
-            foundEntry.status = "❌ Blocked";
-          }
-        }
-      }
-
-      if (tableEntries.length > 0) {
-        tableEntries.sort((a, b) => {
-          if (a.status === "❌ Blocked" && b.status !== "❌ Blocked") {
-            return -1;
-          } else if (a.status !== "❌ Blocked" && b.status === "❌ Blocked") {
-            return 1;
-          } else {
-            return 0;
-          }
-        });
-
-        tableEntries = tableEntries.slice(0, 5); // Limit the table entries to 5
-
-        await core.summary.addRaw(`
-          <h3>🌐 Outbound Network Traffic Analysis</h3>
-          <table>
-            <thead>
-              <tr>
-                <th>PID</th>
-                <th>Process</th>
-                <th>Domain</th>
-                <th>IP Address</th>
-                <th>Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              ${tableEntries
-                .map(
-                  entry => `<tr>
-                  <td>${entry.pid}</td>
-                  <td>${entry.process}</td>
-                  <td>${entry.domain}</td>
-                  <td>${entry.ipAddress}</td>
-                  <td>${entry.status}</td>
-                </tr>`
-                )
-                .join("")}
-            </tbody>
-          </table>`
-        );
+        tableEntries.push({ pid, process, domain, ipAddress, status });
       }
     }
-
-    await core.summary
-      .addRaw(`<p>🔍 <a href="${insights_url}">View detailed insights and policy recommendation</a></p>`)
-      .addSeparator()
-      .write();
   }
+};
+
+export async function addSummary() {
+  if (process.env.STATE_monitorStatusCode !== "200") {
+    return;
+  }
+
+  const web_url = "https://app.stepsecurity.io";
+  const insights_url = `${web_url}/github/${process.env["GITHUB_REPOSITORY"]}/actions/runs/${process.env["GITHUB_RUN_ID"]}`;
+
+  const log = "/home/agent/agent.log";
+  if (!fs.existsSync(log)) {
+    return;
+  }
+
+  const content = fs.readFileSync(log, "utf-8");
+  const lines = content.split("\n");
+
+  let tableEntries = [];
+
+  for (const line of lines) {
+    processLogLine(line, tableEntries);
+  }
+
+  if (tableEntries.length === 0) {
+    return;
+  }
+
+  await core.summary.addSeparator().addRaw(
+    `<h2>GitHub Actions Runtime Security</h2>
+        <p><a href="https://github.com/step-security/harden-runner">By StepSecurity Harden Runner</a></p>`
+  );
+
+  tableEntries.sort((a, b) => {
+    if (a.status === "❌ Blocked" && b.status !== "❌ Blocked") {
+      return -1;
+    } else if (a.status !== "❌ Blocked" && b.status === "❌ Blocked") {
+      return 1;
+    } else {
+      return 0;
+    }
+  });
+
+  tableEntries = tableEntries.slice(0, 5); // Limit the table entries
+
+  await core.summary.addRaw(`
+    <h3>🌐 Outbound Network Traffic Analysis</h3>
+    <table>
+      <thead>
+        <tr>
+          <th>PID</th>
+          <th>Process</th>
+          <th>Domain</th>
+          <th>IP Address</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${tableEntries
+          .map(
+            (entry) => `<tr>
+            <td>${entry.pid}</td>
+            <td>${entry.process}</td>
+            <td>${entry.domain}</td>
+            <td>${entry.ipAddress}</td>
+            <td>${entry.status}</td>
+          </tr>`
+          )
+          .join("")}
+      </tbody>
+    </table>`);
+
+  await core.summary
+    .addRaw(
+      `<p>🔍 <a href="${insights_url}">View detailed insights and policy recommendation</a></p>`
+    )
+    .addSeparator()
+    .write();
 }
 
 export const STATUS_HARDEN_RUNNER_UNAVAILABLE = "409";
