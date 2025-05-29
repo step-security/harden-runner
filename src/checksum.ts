42a3378d67e9db7959e68edd697b039f635d1280
Varun Sharma <varunsh@stepsecurity.io>
2025-05-28 18:06:34 -0700

    update agent version

--- src/checksum.ts
+++ src/checksum.ts
@@ -4,8 +4,8 @@ import * as fs from "fs";
 
 const CHECKSUMS = {
   tls: {
-    amd64: "fcfcba43b5a7e1e4d5f8959e502c67883b22d6ab7a608c70a07d5a2454bce6fd", // v1.6.6
-    arm64: "17a392ff3715d88bc6e9f55895ea9f65b57004a9ae39f520e750ae27c50e0f0c",
+    amd64: "19f9609508c3a9ded9e766babb5a05986d5641263d2f497c2012c8550e7e1176", // v1.6.9
+    arm64: "dd122c47149b666726c2534aa5cb88b1baf1bc9e0519c9919839936b02cc5552",
   },
   non_tls: {
     amd64: "336093af8ebe969567b66fd035af3bd4f7e1c723ce680d6b4b5b2a1f79bc329e", // v0.14.2
