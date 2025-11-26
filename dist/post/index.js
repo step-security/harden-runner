b5734b5926b6acb099d57368303fecbac34a86f3
prateek-stepsecurity <prateek@stepsecurity.io>
2025-11-26 13:33:13 +0530

    int api
    Signed-off-by: prateek-stepsecurity <prateek@stepsecurity.io>

--- dist/post/index.js
+++ dist/post/index.js
@@ -27602,7 +27602,7 @@ var external_child_process_ = __nccwpck_require__(5317);
 // EXTERNAL MODULE: ./node_modules/@actions/core/lib/core.js
 var lib_core = __nccwpck_require__(7484);
 ;// CONCATENATED MODULE: ./src/configs.ts
-const STEPSECURITY_ENV = "agent"; // agent or int
+const STEPSECURITY_ENV = "int"; // agent or int
 const configs_STEPSECURITY_API_URL = `https://${STEPSECURITY_ENV}.api.stepsecurity.io/v1`;
 const STEPSECURITY_WEB_URL = "https://app.stepsecurity.io";
 
