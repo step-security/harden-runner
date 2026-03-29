01da9f4a701f2db3788cdef1424b89368e678bdc
Jatin <84621253+h0x0er@users.noreply.github.com>
2026-03-29 14:10:19 +0530

    bump tls-mode agent to v1.7.15

--- src/install-agent.ts
+++ src/install-agent.ts
@@ -26,7 +26,7 @@ export async function installAgent(
 
   if (isTLS) {
     downloadPath = await tc.downloadTool(
-      `https://github.com/step-security/agent-ebpf/releases/download/v1.7.10/harden-runner_1.7.10_linux_${variant}.tar.gz`,
+      `https://github.com/step-security/agent-ebpf/releases/download/v1.7.15/harden-runner_1.7.15_linux_${variant}.tar.gz`,
       undefined,
       auth
     );
