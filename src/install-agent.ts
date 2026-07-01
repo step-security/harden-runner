f249306c75fd4ab83325ad367a93283d9a4616e6
Varun Sharma <varunsh@stepsecurity.io>
2026-07-01 06:57:34 -0700

    Update Windows agent to v1.0.7

--- src/install-agent.ts
+++ src/install-agent.ts
@@ -226,7 +226,7 @@ export async function installWindowsAgent(configStr: string): Promise<boolean> {
   const agentExePath = path.join(agentDir, "agent.exe");
 
   const downloadPath = await tc.downloadTool(
-    `https://github.com/step-security/agent-releases/releases/download/v1.0.2-win/harden-runner-agent-windows_1.0.2_windows_amd64.tar.gz`,
+    `https://github.com/step-security/agent-releases/releases/download/v1.0.7-win/harden-runner-agent-windows_1.0.7_windows_amd64.tar.gz`,
     undefined,
     auth
   );
